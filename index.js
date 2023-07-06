15ae56c3263737f269d5dba4eb7db16c8544eb37
HARSHVARDHAN SHAMRAO BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2023-07-06 11:12:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -128,3 +128,53 @@ window.addEventListener("keypress", event => {
         inputDir.y = 0;
     }
 });
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            inputDir.x = 1;
+            inputDir.y = 0;
+        } else {
+            inputDir.x = -1;
+            inputDir.y = 0;
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            inputDir.x = 0;
+            inputDir.y = 1; 
+        } else { 
+            inputDir.x = 0;
+            inputDir.y = -1;
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
